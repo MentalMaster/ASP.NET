edbd0b71d45cf5e821dba224a9128cadcfd52c85
MentalMaster <rothomzoom@gmail.com>
2022-03-21 09:50:23 +0300

    Lab01.ex03_MVC

---
+++ Lab01.ex03._MVC/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Lab01.ex02_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01.ex02_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(string hel) 
+        { 
+            string Greeting = ModelClass.ModelHello() + ", " + hel; 
+            return Greeting;
+        }
+
+    }
+}
