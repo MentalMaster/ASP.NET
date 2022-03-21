ca82593fd5da1588aa14ea3e2fa96f8ca1d50944
MentalMaster <rothomzoom@gmail.com>
2022-03-21 08:25:13 +0300

    Lab05.ex01

---
+++ Lab05.ex01/Reg.aspx.designer.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Lab01.ex01
+{
+
+
+    public partial class Reg
+    {
+        protected global::System.Web.UI.WebControls.ValidationSummary validationSummary;
+        protected global::System.Web.UI.WebControls.TextBox name;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        protected global::System.Web.UI.WebControls.TextBox email;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator4;
+        protected global::System.Web.UI.WebControls.TextBox phone;
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
+        protected global::System.Web.UI.WebControls.CheckBox CheckBoxYN;
+    }
+}
---
+++ Lab05.ex01/Site.Master.designer.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Lab01.ex01
+{
+
+
+    public partial class Site
+    {
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.Menu NavigationMenu;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+    }
+}
