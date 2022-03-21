0cce83cd01d4f17c59c23b1ec5fde0cfd930adac
MentalMaster <rothomzoom@gmail.com>
2022-03-21 09:43:02 +0300

    Lab01.ex02_MVC

---
+++ Lab01.ex02_MVC/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01.ex02_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public string Index(string hel)
+        {
+            int hour = DateTime.Now.Hour;
+            string Greeting = hour < 12 ? "Доброе утро" : "Добрый день" + ", " + hel;
+            return Greeting;
+        }
+        
+    }
+}
