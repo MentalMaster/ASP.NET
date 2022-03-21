e552b86c8bed22a83fee2b8327720cd793d2aee3
MentalMaster <rothomzoom@gmail.com>
2022-03-21 09:07:17 +0300

    Lab01.ex01_MVC

---
+++ Lab01.ex01_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab01.ex01_MVC.MvcApplication" Language="C#" %>
---
+++ Lab01.ex01_MVC/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Lab01.ex01_MVC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab01.ex01_MVC")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("38e0cf1e-5977-4abd-b4d4-e508dfc0c097")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
