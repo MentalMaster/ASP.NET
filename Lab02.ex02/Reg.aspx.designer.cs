0d9a8a5f8996f32ebd5ff4d69c9b0d62b87074b6
MentalMaster <rothomzoom@gmail.com>
2022-03-21 07:53:40 +0300

    Lab02.ex02

---
+++ Lab02.ex02/Reg.aspx.designer.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Lab01.ex01
+{
+
+
+    public partial class Reg
+    {
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.TextBox name;
+        protected global::System.Web.UI.WebControls.TextBox email;
+        protected global::System.Web.UI.WebControls.TextBox phone;
+        protected global::System.Web.UI.WebControls.CheckBox CheckBoxYN;
+    }
+}
